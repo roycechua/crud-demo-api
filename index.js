b4aaa539753902fdeb6aaca6a4279c6f7ca36f1a
roycechua23 <royce236@gmail.com>
2021-11-29 09:40:41 +0800

    Refactors

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded())
 
-let data = [
-    { id: 1, title: "React Native Seminar Today", story: "Today's the workshop", author: "Royce Chua" },
-]
+let id = 0
+
+let data = []
 
 let successMessage = (message, data) => ({
     message,
@@ -21,7 +21,7 @@ let errorMessage = (message) => ({
 // Fetch all
 app.get('/', (req, res) => res.send(data))
 
-// Search for a specific post
+// Search for a specific note
 app.get('/search', (req, res) => {
     let result = data.filter(d => d.id == req.query.id);
     if (result.length > 0) {
@@ -30,36 +30,37 @@ app.get('/search', (req, res) => {
     return res.status(404).json(errorMessage("Record not found"));
 })
 
-// Add a post object to the list
-app.post('/posts', (req, res) => {
-    if(req.body.data && req.body.data.id && req.body.data.title && req.body.data.story) {
-        data.push(req.body.data)
-        return res.status(200).json(successMessage("Add post successful", req.body.data))
+// Add a note object to the list
+app.post('/notes', (req, res) => {
+    if(req.body.data && req.body.data.note) {
+        const newNote = {id: ++id, note: req.body.data.note};
+        data.push(newNote)
+        return res.status(200).json(successMessage("Add note successful", newNote))
     } 
     
     return res.status(422).send(errorMessage('Unprocessable Entity'))
 })
 
-// delete a post object to the list
-app.delete('/posts/:id', (req, res) => {
+// delete a note object to the list
+app.delete('/notes/:id', (req, res) => {
     if(req.params.id) {
         let new_data = data.filter(d => d.id != req.params.id)
         if(new_data.length != data.length) {
             data = new_data;
-            return res.status(200).send(successMessage('Delete post success'))
+            return res.status(200).send(successMessage('Delete note success'))
         } 
 
-        return res.status(404).send(errorMessage('Post record was not found'))
+        return res.status(404).send(errorMessage('Note record was not found'))
     } 
     
     return res.status(422).send(errorMessage('Unprocessable Entity'))
 })
 
-app.put('/posts/:id', (req, res) => {
+app.put('/notes/:id', (req, res) => {
   if(req.params.id && req.body.data) {
     data = [...data.filter(d => d.id != req.params.id), req.body.data ];
     // console.log(data)
-    return res.status(200).send(successMessage('Update post success', req.body.data))
+    return res.status(200).send(successMessage('Update note success', req.body.data))
   }  
 
   return res.status(422).send(errorMessage('Unprocessable Entity'))
